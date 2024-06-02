c1b4f9c727a15b35d7ade2edf3077698a394ab03
DCODL <dd@dd.com>
2024-06-02 19:26:11 +0200

    - pridani game over obrazovky

--- survivors_game.gd
+++ survivors_game.gd
@@ -1,10 +1,5 @@
 extends Node2D
 
-func _ready():
-	spawn_mob()
-	spawn_mob()
-	spawn_mob()
-	spawn_mob()
 
 func spawn_mob():
 	var new_mob = preload("res://mob.tscn").instantiate()
@@ -12,6 +7,10 @@ func spawn_mob():
 	new_mob.global_position = %PathFollow2D.global_position
 	add_child(new_mob)
 
-
 func _on_timer_timeout():
 	spawn_mob()
+
+
+func _on_player_health_depleted():
+	%GameOver.visible = true
+	get_tree().paused = true
