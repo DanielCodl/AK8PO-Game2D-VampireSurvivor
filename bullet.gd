8e6c936ec1b49424d2c367ca8b662a3089011210
DCODL <dd@dd.com>
2024-06-02 16:50:09 +0200

    - pridani Signalu (body_entered) pro poskozeni padoucha vystrelenou kulkou - pridani funkce shoot do nodu Gun pro metodu vystreleni kulky

--- bullet.gd
+++ bullet.gd
@@ -1,6 +1,5 @@
 extends Area2D
 
-
 var travelled_distance = 0
 
 func _physics_process(delta):
@@ -13,4 +12,9 @@ func _physics_process(delta):
 	travelled_distance += SPEED * delta
 	if travelled_distance > RANGE:
 		queue_free()
-		
+
+
+func _on_body_entered(body):
+	queue_free()
+	if body.has_method("take_damage"):
+		body.take_damage()
